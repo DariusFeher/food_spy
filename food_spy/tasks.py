9c35904d453d48e446561da451870cb0b529b118
Darius Feher <darius.feher@student.manchester.ac.uk>
2022-02-02 21:08:29 +0000

    Add dummy repeating task

--- food_spy/tasks.py
+++ food_spy/tasks.py
@@ -1,12 +1,9 @@
 from background_task import background
 from django.core.mail import send_mail
-from post_office.connections import connections
-
-
 
 @background()
 def send_notif_email():
     print("INSIDE")
-    
+
     print("HERE")
     return 'Email sent!'
