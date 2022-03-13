45f9e96e99adc6a01a138af07591a4fa682da202
Darius Feher <darius.feher@student.manchester.ac.uk>
2022-03-13 22:29:46 +0000

    Add unit tests for recipes and supermarkets data

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,98 @@
 from django.test import TestCase
 
+from users.models import Account
+from .models import Recipe
+
 # Create your tests here.
+class TestUserForms(TestCase):
+    ## ----------- TEST MODELS --------------
+    def test_successfully_create_recipe_with_valid_data(self):
+        tesco =  [1, 2, 3]
+        british_on = [2, 3, 4]
+        # create user
+        user = Account.objects.create(username='testUSER', email='test2@test.com', is_email_verified=True)
+        recipe = Recipe.objects.create(
+                user=user,
+                products_tesco=tesco,
+                products_british_online_supermarket=british_on,
+        )
+        retrieved_recipe = Recipe.objects.get(pk=recipe.pk)
+        self.assertEqual(retrieved_recipe.user, user)
+        self.assertEqual(retrieved_recipe.products_tesco, tesco)
+        self.assertEqual(retrieved_recipe.products_british_online_supermarket, british_on)
+
+    def test_save_recipe_invalid_user_not_allowed(self):
+        tesco =  [1, 2, 3]
+        british_on = [2, 3, 4]
+        try:
+            recipe = Recipe.objects.create(
+                    user='usertest',
+                    products_tesco=tesco,
+                    products_british_online_supermarket=british_on,
+            )
+            self.fail()
+        except:
+            pass
+    
+    def test_save_recipe_no_user_not_allowed(self):
+        tesco =  [1, 2, 3]
+        british_on = [2, 3, 4]
+        try:
+            recipe = Recipe.objects.create(
+                    products_tesco=tesco,
+                    products_british_online_supermarket=british_on,
+            )
+            self.fail()
+        except:
+            pass
+    
+    def test_save_recipe_invaliv_products_tesco_not_allowed(self):
+        british_on = [2, 3, 4]
+        user = Account.objects.create(username='testUSER', email='test2@test.com', is_email_verified=True)
+        try:
+            recipe = Recipe.objects.create(
+                    user=user,
+                    products_tesco='das',
+                    products_british_online_supermarket=british_on,
+            )
+            self.fail()
+        except:
+            pass
+
+    def test_save_recipe_no_products_tesco_not_allowed(self):
+        british_on = [2, 3, 4]
+        user = Account.objects.create(username='testUSER', email='test2@test.com', is_email_verified=True)
+        try:
+            recipe = Recipe.objects.create(
+                    user=user,
+                    products_british_online_supermarket=british_on,
+            )
+            self.fail()
+        except:
+            pass
+    
+    def test_save_recipe_no_products_british_online_supermarket_not_allowed(self):
+        tesco = [2, 3, 4]
+        user = Account.objects.create(username='testUSER', email='test2@test.com', is_email_verified=True)
+        try:
+            recipe = Recipe.objects.create(
+                    user=user,
+                    products_tesco=tesco,
+            )
+            self.fail()
+        except:
+            pass
+
+    def test_save_recipe_invalid_products_british_online_supermarket_not_allowed(self):
+        tesco = [2, 3, 4]
+        user = Account.objects.create(username='testUSER', email='test2@test.com', is_email_verified=True)
+        try:
+            recipe = Recipe.objects.create(
+                    user=user,
+                    products_tesco=tesco,
+                    products_british_online_supermarket='random',
+            )
+            self.fail()
+        except:
+            pass
+
--- supermarkets_data/tests.py
+++ supermarkets_data/tests.py
@@ -1,3 +1,129 @@
 from django.test import TestCase
 
+from .models import TescoData, BritishOnlineSupermarketData
+
 # Create your tests here.
+class TestUserForms(TestCase):
+    ## ----------- TEST MODELS --------------
+    # TESCO DATA TESTS
+    def test_success_save_tesco_data_with_valid_data(self):
+        tokens = [1, 2, 3]
+        products = [0, 5, 15]
+        tesco_data = TescoData.objects.create(
+                protected_tokens = tokens,
+                products_data = products
+        )
+        retrieved_tesco_data = TescoData.objects.get(pk=tesco_data.pk)
+        self.assertEqual(retrieved_tesco_data.protected_tokens, tokens)
+        self.assertEqual(retrieved_tesco_data.products_data, products)
+    
+    def test_save_tesco_data_with_invalid_data_not_allowed(self):
+        try:
+            tesco_data = TescoData.objects.create(
+                    protected_tokens = 'random_token',
+                    products_data = 'random_data'
+            )
+            self.fail()
+        except:
+            pass
+    
+    def test_save_tesco_data_with_empty_data_not_allowed(self):
+        try:
+            tesco_data = TescoData.objects.create(
+            )
+            self.fail()
+        except:
+            pass
+    
+    def test_save_tesco_data_with_empty_tokens_not_allowed(self):
+        try:
+            products = [0, 5, 15]
+            tesco_data = TescoData.objects.create(
+                    products_data = products
+            )
+            self.fail()
+        except:
+            pass
+    
+    def test_save_tesco_data_with_empty_products_not_allowed(self):
+        try:
+            tokens = [1, 2, 3]
+            tesco_data = TescoData.objects.create(
+                    protected_tokens = tokens,
+            )
+            self.fail()
+        except:
+            pass
+    
+    # BRITISH ONLINE SUPERMARKET DATA TESTS
+
+    def test_success_save_british_online_supermarket_data_with_valid_data(self):
+        tokens = [1, 2, 3]
+        products = [0, 5, 15]
+        entities = [15, 12, 12]
+        british_data = BritishOnlineSupermarketData.objects.create(
+                protected_tokens = tokens,
+                products_data = products,
+                products_entities = entities,
+        )
+        retrieved_british_data = BritishOnlineSupermarketData.objects.get(pk=british_data.pk)
+        self.assertEqual(retrieved_british_data.protected_tokens, tokens)
+        self.assertEqual(retrieved_british_data.products_data, products)
+        self.assertEqual(retrieved_british_data.products_entities, entities)
+
+    
+    def test_save_british_online_supermarket_data_with_invalid_data_not_allowed(self):
+        try:
+            british_data = BritishOnlineSupermarketData.objects.create(
+                    protected_tokens = 'random_token',
+                    products_data = 'random_data',
+                    entities = 'random_entity'
+            )
+            self.fail()
+        except:
+            pass
+    
+    def test_save_british_online_supermarket_data_with_empty_data_not_allowed(self):
+        try:
+            british_data = BritishOnlineSupermarketData.objects.create(
+            )
+            self.fail()
+        except:
+            pass
+    
+    def test_save_british_online_supermarket_data_with_empty_tokens_not_allowed(self):
+        try:
+            products = [0, 5, 15]
+            entities = [15, 12, 12]
+            british_data = BritishOnlineSupermarketData.objects.create(
+                    products_data = products,
+                    products_entities = entities,
+            )
+            self.fail()
+        except:
+            pass
+    
+    def test_save_british_online_supermarket_data_with_empty_products_data_not_allowed(self):
+        try:
+            tokens = [1, 2, 3]
+            entities = [15, 12, 12]
+            british_data = BritishOnlineSupermarketData.objects.create(
+                    protected_tokens = tokens,
+                    products_entities = entities,
+            )
+            self.fail()
+        except:
+            pass
+    
+    def test_save_british_online_supermarket_data_with_empty_products_entities_not_allowed(self):
+        try:
+            tokens = [1, 2, 3]
+            products = [0, 5, 15]
+            british_data = BritishOnlineSupermarketData.objects.create(
+                    protected_tokens = tokens,
+                    products_data = products,
+            )
+            self.fail()
+        except:
+            pass
+
